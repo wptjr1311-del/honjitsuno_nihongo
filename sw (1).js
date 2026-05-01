5097a4804cde6234eca518e2841d013960e49f11
wptjr1311-del <wptjr1311@gmail.com>
2026-05-01 18:37:09 +0900

    Delete sw (1).js

--- sw (1).js
+++
@@ -1,8 +0,0 @@
-// SW 비활성화 — 캐시 전부 삭제 후 즉시 종료
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
-});
